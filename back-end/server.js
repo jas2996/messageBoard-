0c7fca8d6e45ae24df884842d110ceb488ff48d4
Jaskiran Lamba <jlamba@Jaskirans-MacBook-Pro.local>
2017-05-02 17:01:15 -0400

    added mongoose database to track posts

--- back-end/server.js
+++ back-end/server.js
@@ -1,18 +1,48 @@
- var express = require('express');
- var app = express();
- var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//var database;
+var Message = mongoose.model('Message',{
+	msg: String
+});
 
 app.use(bodyParser.json())
 
 app.use(function(req,res,next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
 })
 
 app.post('/api/message', function(req,res){
 	console.log(req.body);
+
+	var message = new Message(req.body);
+
+	message.save();
+	//database.collection('messages').insertOne(req.body);
 	res.status(200);
+})
+
+function getMessage(){
+	Message.find({}).exec(function(err,result){
+		console.log(result)
+	})
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err, db){
+	if (!err){
+		console.log("we are connected to mongo");
+		//database = db;
+		//db.collection('messages').insertOne({'msg':'test'});
+		getMessage();
+	}else{
+		console.log("we are not connected")
+	}
+
+
 })
 
  var server = app.listen(5000,function(){
